5db6cb6be2a5bec1f057bc6e88be7dc758328f75
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-06 10:18:08 -0600

    added class updates with different colors

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const table = document.querySelector('table');
 const addRowButton = document.querySelector('#add-row');
+const selectElement = document.querySelector('select');
 
 const numberOfColumns = 20;
+let currentColorClass = 'redCell';
 
 function makeRow() {
   const newRow = document.createElement('tr');
@@ -18,7 +20,21 @@ addRowButton.addEventListener('click', makeRow);
 
 function colorize(evt) {
   const cell = evt.target;
-  cell.classList.toggle('aquaCell')
+  const cellClassList = cell.classList;
+
+  if (cellClassList.contains(currentColorClass)) {
+    cellClassList.remove(currentColorClass)
+  } else {
+    cellClassList.add(currentColorClass);
+  }
 }
 
 table.addEventListener('click', colorize);
+
+
+
+function selectorChange(evt) {
+  currentColorClass = evt.target.value;
+}
+
+selectElement.addEventListener('change', selectorChange)
